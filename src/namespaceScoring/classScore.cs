71c1677163eddfca9dca241e146894e0736c2b72
JAKE TUNALEY <jtunaley@gmail.com>
2016-05-04 17:36:57 +1000

    Moved Score and ScoreCollection to namespace SnakeGame.Scoring

---
+++ src/namespaceScoring/classScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SnakeGame.Scoring
+{
+	public class Score
+	{
+		public Score(Difficulty gameplayDifficulty,string playerName, int value)
+		{
+			GameplayDifficulty = gameplayDifficulty;
+			PlayerName = playerName;
+			Value = value;
+		}
+		public Score(Difficulty gameplayDifficulty, int value) : this(gameplayDifficulty, "", value)
+		{
+		}
+		public Score(Difficulty gameplayDifficulty, string playerName) : this(gameplayDifficulty, playerName,0)
+		{
+		}
+		public Score(Difficulty gameplayDifficulty) : this(gameplayDifficulty, 0)
+		{
+		}
+		public Difficulty GameplayDifficulty
+		{
+			get;
+			private set;
+		}
+		public string PlayerName
+		{
+			get;
+			set;
+		}
+		public int Value
+		{
+			get;
+			set;
+		}
+	}
+}
+
